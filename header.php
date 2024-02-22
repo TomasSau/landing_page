905b3886ac521507aa6573bc963da6c2ed90c385
TomasSau <tomas.sauciunas@gmail.com>
2024-02-22 06:50:24 +0200

    First Commit

---
+++ header.php
@@ -0,0 +1,7 @@
+<!-- Headeris su puslapio informacija -->
+<div id="header">
+    <h1>Sveiki atvykę į Landing Page</h1>
+    <p>Čia rasite informaciją apie mus.</p>
+    <!-- Nuoroda į kitą PHP failą su papildoma informacija -->
+    <a href="apie_programa.php">Apie programą</a>
+</div>
